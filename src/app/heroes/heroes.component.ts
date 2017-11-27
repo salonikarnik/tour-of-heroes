ac9d50ce10f5724b1c0f88d980ce81e8e4121b05
salonikarnik <salonikarnik21@gmail.com>
2017-11-26 19:50:13 -0600

    feat: add service for hero component

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import {HEROES} from '../mock-heroes';
+import {HeroService} from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -16,11 +16,16 @@ export class HeroesComponent implements OnInit {
     this.selectedHero=hero;
   }
 
-  heroes = HEROES;
+  heroes: Hero[];
 
-  constructor() { }
+  getHeroes():void{
+    this.heroes = this.heroService.getHeroes();
+  }
+
+  constructor(private heroService: HeroService) { }
 
   ngOnInit() {
+    this.getHeroes();
   }
 
 }
